0a9f7aee83cb10ca612870a9327dd998f0283b67
FernHerrera11 <holaninamaru@gmail.com>
2025-06-10 11:07:25 -0700

    fhasfd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navigation from './components/Navigation/navigation.component'
 import Blog from './components/Blog/blog.component'
-import Logo from './components/Logo/logo.component'
+// import Logo from './components/Logo/logo.component'
 import {Route, Routes} from 'react-router-dom'
 import About from './components/About/about.component'
 import Contact from './components/Contact/contact.component';
--- src/components/Logo/logo.component.jsx
+++ src/components/Logo/logo.component.jsx
@@ -1,14 +1,14 @@
 
-import reactLogo from '../../assets/react.svg';
+// import reactLogo from '../../assets/react.svg';
 
 const Logo = () => {
 
     return(
         <div>
-         
+{/*          
             <a href="https://react.dev" target="_blank">
               <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+            </a> */}
           
           <h1>growFernGrow</h1>
 
